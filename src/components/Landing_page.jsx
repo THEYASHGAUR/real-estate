b212d77b934609cdc31478242326279d6ff903be
Yash Gaur <yashgaur2525@gmail.com>
2023-08-13 01:53:01 +0530

    input button

--- src/components/Landing_page.jsx
+++ src/components/Landing_page.jsx
@@ -7,7 +7,7 @@ const Landing_page = () => {
       <div className=" bg-landing_page h-110  bg-red-500 flex flex-col items-center justify-center">
         <p className="text-4xl font-bold text-black">Agents. Tours. Loans. Homes.</p>
         {/* serach bar */}
-        <div className='w-96 relative mt-10 sm:w-110'>
+        <div className='w-96 relative mt-10 sm:w-110 sm:mx-3'>
           <input
             className='w-full h-20 text-2xl p-3 pr-28 border-spacing-1 space-x-2  overflow-hidden'
             type="text"
